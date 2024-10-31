3ed41d60ad928c38e820423f9e04f188fa53b7dc
Yigit <yigistaff@gmail.com>
2024-10-31 15:40:10 -0400

    initial commit.

---
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -0,0 +1,16 @@
+/*
+
+@Override
+public double getTotalPrice() {
+        return (getVehicleSold().getPrice() - expectedEndingValue) + leaseFee;
+        }
+
+@Override
+public double getMonthlyPayment() {
+        int numberOfPayments = 36;
+        double interestRate = 4.0 / 1200;
+        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+        monthlyPayment = Math.round(monthlyPayment * 100);
+        monthlyPayment /= 100;
+        return monthlyPayment;
+        } */
