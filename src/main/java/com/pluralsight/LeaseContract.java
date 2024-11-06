16e8309a2aff7d5a442e78b7cdefb285231143a6
Yigit <yigistaff@gmail.com>
2024-11-06 01:48:48 -0500

    LeaseContract class has been added and phase 2 has been completed.

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,16 +1,35 @@
-/*
+package com.pluralsight;
 
-@Override
-public double getTotalPrice() {
+public class LeaseContract extends Contract {
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        super(date, customerName, customerEmail, vehicleSold);
+        this.expectedEndingValue = vehicleSold.getPrice() * 0.5;
+        this.leaseFee = vehicleSold.getPrice() * 0.07;
+    }
+
+    public double getExpectedEndingValue() {
+        return expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    @Override
+    public double getTotalPrice() {
         return (getVehicleSold().getPrice() - expectedEndingValue) + leaseFee;
-        }
+    }
 
-@Override
-public double getMonthlyPayment() {
+    @Override
+    public double getMonthlyPayment() {
         int numberOfPayments = 36;
         double interestRate = 4.0 / 1200;
         double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
         monthlyPayment = Math.round(monthlyPayment * 100);
         monthlyPayment /= 100;
         return monthlyPayment;
-        } */
+    }
+}
