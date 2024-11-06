de97ca3a8bc0e1cf3f1f9865efbd24b0f386d32b
Yigit <yigistaff@gmail.com>
2024-11-05 20:19:12 -0500

    Contract class has been modified.

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
 public class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public double getTotalPrice() {
+        return vehicleSold.getPrice();
+    }
+
+    public double getMonthlyPayment() {
+        return 0.0; // Default implementation, will be overridden in subclasses
+    }
 }
